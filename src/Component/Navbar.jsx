ad4e98507fad97a2d7c1799295b9525a1bfa01c2
Shadan9113 <mdshadanzaheer@gmail.com>
2024-11-07 11:38:13 +0530

    Initial commit

---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <nav>
+            <div>
+                <h2>code with shadan</h2>
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Signup</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
